fc7dd29711ea5b32f4474e1f859e23a099bd132c
Michelle Elias Flores <michelleeliasflores@gmail.com>
2022-08-04 11:33:49 -0400

    added a ton of things to budgeting

--- kwkgroup1finalproject/ViewControllerbudget.swift
+++ kwkgroup1finalproject/ViewControllerbudget.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class ViewControllerbudget: UIViewController {
 
+    @IBOutlet weak var budgetbutton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func budgetbutton(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
